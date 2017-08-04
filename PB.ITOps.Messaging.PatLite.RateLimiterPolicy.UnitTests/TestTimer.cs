ca031e2f3c68d0859600c05c68195d7c6e86c88e
Andrew Bambury <andrew.bambury@purplebricks.com>
2017-08-04 15:21:50 +0100

    PatLite.Encryption added to support encrypted message de-serialisation - Refactored Unit tests to split RateLimiterPolicy unit tests and core PatLite unit tests - Updated projects to require version of PatLite with message processing policy fix.

---
+++ PB.ITOps.Messaging.PatLite.RateLimiterPolicy.UnitTests/TestTimer.cs
@@ -0,0 +1,30 @@
+﻿namespace PB.ITOps.Messaging.PatLite.RateLimiterPolicy.UnitTests
+{
+    public class TestTimer : ITimer
+    {
+        private long _elapsedTime = -1;
+
+        public void AddElapsedSeconds(int value)
+        {
+            _elapsedTime += value * 1000;
+        }
+
+        public void AddElapsedMilliseconds(int value)
+        {
+            _elapsedTime += value;
+        }
+
+        public void Start()
+        {
+            if (_elapsedTime == -1)
+            {
+                _elapsedTime = 0;
+            }
+        }
+
+        public long ElapsedMilliseconds
+        {
+            get => _elapsedTime; set => _elapsedTime = value;
+        }
+    }
+}

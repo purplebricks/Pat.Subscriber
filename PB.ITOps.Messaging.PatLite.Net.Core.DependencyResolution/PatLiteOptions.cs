7815fe74618d1b2343379d2b13c4fb5f514ddade
Andrew Bambury <andrew.bambury@purplebricks.com>
2017-08-23 16:24:19 +0100

    Fix missing dependencies: - IMessageDeserialiser: default added with ability to customise via options - added missing messagePolicyBuilder.RegisterPolicies call to register individual policies within policy chain - xml documentation added.

--- PB.ITOps.Messaging.PatLite.Net.Core.DependencyResolution/PatLiteOptions.cs
+++ PB.ITOps.Messaging.PatLite.Net.Core.DependencyResolution/PatLiteOptions.cs
@@ -1,10 +1,35 @@
+using System;
+using PB.ITOps.Messaging.PatLite.Serialiser;
+
 namespace PB.ITOps.Messaging.PatLite.Net.Core.DependencyResolution
 {
     public class PatLiteOptions
     {
+        /// <summary>
+        /// Required
+        /// </summary>
         public SubscriberConfiguration SubscriberConfiguration { get; set; }
+        /// <summary>
+        /// Optional 
+        /// by default monitoring and standard processing polciies will be used
+        /// </summary>
         public PatLiteGlobalPolicyBuilder GlobalPolicyBuilder { get; set; }
+        /// <summary>
+        /// Optional
+        /// Scanner can be used to register handlers
+        /// Alternatively can call helper function ServiceCollection.AddHandlersFromAssemblyContainingType<>
+        /// </summary>
         public AssemblyScanner AssemblyScanner { get; set; }
+        /// <summary>
+        /// Optional
+        /// by default DefaultMessageProcessingPolicy will be used
+        /// </summary>
         public PatLiteMessagePolicyBuilder MessagePolicyBuilder { get; set; }
+
+        /// <summary>
+        /// Optional
+        /// by default NewtonsoftMessageDeserialiser will be used
+        /// </summary>
+        public Func<IServiceProvider, IMessageDeserialiser> MessageDeserialiser { get; set; }
     }
 }

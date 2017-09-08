86c761e9b7ebfb01860944a1c38358e3d2a8c92b
Paul Manzotti <paul.manzotti@purplebricks.com>
2017-09-08 15:15:13 +0100

    Will transfer any custom properties on the brokered message to the context

--- PB.ITOps.Messaging.PatLite/IoC/IMessageContext.cs
+++ PB.ITOps.Messaging.PatLite/IoC/IMessageContext.cs
@@ -1,8 +1,11 @@
-﻿namespace PB.ITOps.Messaging.PatLite.IoC
+﻿using System.Collections.Generic;
+
+namespace PB.ITOps.Messaging.PatLite.IoC
 {
     public interface IMessageContext
     {
         string CorrelationId { get; set; }
         bool MessageEncrypted { get; set; }
+        IDictionary<string, object> CustomProperties { get; set; }
     }
 }

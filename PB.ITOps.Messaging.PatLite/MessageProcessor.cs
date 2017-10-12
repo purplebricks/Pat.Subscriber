85ee87f3f08111cde87117779b4f612e0d38a4a6
Rob White <rob.j.white@gmail.com>
2017-10-12 17:10:40 +0100

    Handle multiple message payloads

--- PB.ITOps.Messaging.PatLite/MessageProcessor.cs
+++ PB.ITOps.Messaging.PatLite/MessageProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using log4net;
 using Microsoft.ServiceBus.Messaging;
@@ -25,7 +26,7 @@ namespace PB.ITOps.Messaging.PatLite
             using (var scope = _messageDependencyResolver.BeginScope())
             {
                 var messageTypeString = message.Properties["MessageType"].ToString();
-                var messageBody = message.GetBody<string>();
+                var messageBody = await GetMessageBody(message);
                         
                 var correlationId = message.Properties.ContainsKey("PBCorrelationId")
                     ? message.Properties["PBCorrelationId"].ToString()
@@ -59,6 +60,27 @@ namespace PB.ITOps.Messaging.PatLite
                 {
                     await messageProcessingPolicy.OnMessageHandlerFailed(message, messageBody, ex);
                     await globalPolicy.OnMessageHandlerFailed(message, messageBody, ex);
+                }
+            }
+        }
+
+        private static async Task<string> GetMessageBody(BrokeredMessage message)
+        {
+            /*
+             * This is a work around to cope with two clients, one client 
+             * is publishing with a stream payload, the other is with a 
+             * string payload. Once all versions of the PB.ITOps.Messaging.PatSender 
+             * are on 1.5.28 or above this can be removed.
+             */
+            using (var messageStream = message.GetBody<Stream>())
+            {
+                using (var reader = new StreamReader(messageStream))
+                {
+                    var payload = await reader.ReadToEndAsync();
+
+                    var start = payload.IndexOf("{", StringComparison.InvariantCultureIgnoreCase);
+                    var end = payload.LastIndexOf("}", StringComparison.InvariantCultureIgnoreCase) + 1;
+                    return payload.Substring(start, end - start);
                 }
             }
         }

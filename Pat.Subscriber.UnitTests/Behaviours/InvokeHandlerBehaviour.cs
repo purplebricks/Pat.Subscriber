7753f7f56ccc6c10edaa48616aa4a13a3203e525
Andrew Bambury <andrew.bambury@purplebricks.com>
2018-07-12 09:56:35 +0100

    Update InvokeHandlerBehaviour to Unwrap and Re-Throw Exceptions from the Invoked Handler
    InvokeHandlerBehaviour invokes HandleAsync method via reflection, which means that if the handler throws an exception, this will be wrapped in a TargetInvocationException.
    This change allows behaviours within the processing pipeline to catch the original exception thrown from Handler whilst preserving the stack trace, and not have to be concerned with TargetInvocationException and therefore implementation details of how the handler was invoked.

---
+++ Pat.Subscriber.UnitTests/Behaviours/InvokeHandlerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using NSubstitute;
+using Pat.Subscriber.Deserialiser;
+using Pat.Subscriber.IoC;
+using Pat.Subscriber.MessageMapping;
+using Pat.Subscriber.MessageProcessing;
+using Xunit;
+
+namespace Pat.Subscriber.UnitTests.Behaviours
+{
+    public class InvokeHandlerBehaviourTests
+    {
+        public class FakeHandler : IHandleEvent<TestEvent>
+        {
+            public Task HandleAsync(TestEvent message)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class TestEvent
+        {
+        }
+
+
+        [Fact]
+        public async Task  When_InvokedHandlerThrowsException_ThenBehaviourThrowsUnwrappedException()
+        {
+            var invokeHandlerBehaviour = Substitute.ForPartsOf<InvokeHandlerBehaviour>();
+            invokeHandlerBehaviour.GetHandlerForMessageType(Arg.Any<Message>())
+                .ReturnsForAnyArgs(new MessageTypeMapping(typeof(TestEvent), typeof(FakeHandler)));
+
+            var messageDependencyScope = Substitute.For<IMessageDependencyScope>();
+            messageDependencyScope.GetService(null).ReturnsForAnyArgs(new FakeHandler());
+            messageDependencyScope.GetService<IMessageDeserialiser>().Returns(new NewtonsoftMessageDeserialiser());
+           var messageContext = new MessageContext
+            {
+                Message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new TestEvent()))),
+                DependencyScope = messageDependencyScope
+            };
+
+            await Assert.ThrowsAsync<NotImplementedException>(() =>
+            {
+                return invokeHandlerBehaviour.Invoke(context => Task.CompletedTask, messageContext);
+            });
+        }
+    }
+}
